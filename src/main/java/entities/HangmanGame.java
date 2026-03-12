567bc6f18e7dfeae7324b56a45ffed8fdae0ec14
QuentinW59 <quentin.wisniewski@outlook.fr>
2026-03-12 15:44:45 +0100

    fin des tests : tout est ok

--- src/main/java/entities/HangmanGame.java
+++ src/main/java/entities/HangmanGame.java
@@ -26,11 +26,33 @@ public class HangmanGame {
     }
 
     public boolean testChar(char letter){
-        return false;
+        char upperCaseLetter = Character.toUpperCase(letter);// on definit aue la lettre est majuscule
+        if(!attempts.contains(upperCaseLetter)){
+            attempts.add(upperCaseLetter);// on ajoute les lettres utilises
+        }
+        // si on ne trouve pas la lettre donc inferieur a 0 on perd une vie si non premiere occurence trouvé on continue
+        if(wordToGuess.toUpperCase().indexOf(upperCaseLetter) >= 0){
+            updateMask(upperCaseLetter);
+            return true;
+        } else {
+            lives--;
+            return false;
+        }
+    }
+
+    private void updateMask(char letter){
+        StringBuilder newMask = new StringBuilder(mask);
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            // si la lettre est en double ou triple la boucle mettra le mask a jour avec le nombre de lettres correspondant
+            if(Character.toUpperCase(wordToGuess.charAt(i)) == letter){
+                newMask.setCharAt(i, wordToGuess.charAt(i));
+            }
+        }
+        this.mask = newMask.toString();
     }
 
     public boolean testWin(){
-        return false;
+        return lives > 0 && !mask.contains("_");
     }
 
 }
--- src/main/java/entities/WordGenerator.java
+++ src/main/java/entities/WordGenerator.java
@@ -1,11 +1,14 @@
 package entities;
 import lombok.*;
 
+import java.util.Random;
+
 @Data
 public class WordGenerator {
     private static final String[] _word = {"JAVA", "CESI", "UTOPIOS", "EXAMEN","DEVELOPPEMENT"};
 
     public String generateWord(){
-        return "";
+        int randomIndex = new Random().nextInt(_word.length);
+        return _word[randomIndex];
     }
 }
